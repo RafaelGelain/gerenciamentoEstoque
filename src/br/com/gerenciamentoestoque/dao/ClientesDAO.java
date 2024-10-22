99a3339a3824294736eaf093c5b2936c845fa2a2
Rafael Augusto Gelain <rafael.augustogelain2@gmail.com>
2024-10-21 22:34:03 -0300

    feature: Ajustes de erro da interface / adicionado edicação/exclusão no cadastro.

--- src/br/com/gerenciamentoestoque/dao/ClientesDAO.java
+++ src/br/com/gerenciamentoestoque/dao/ClientesDAO.java
@@ -19,7 +19,7 @@ public class ClientesDAO {
     public void Salvar(Clientes obj){
         try {
             //Insert SQL
-            String sql = "insert into tb_clientes (nome, rg, cpf, email, telefone, celular, cep, endereco, numero, complemento, bairro, cidade, estado)"
+            String sql = "insert into clientes (nome, rg, cpf, email, telefone, celular, cep, endereco, numero, complemento, bairro, cidade, estado)"
                     + "values(?,?,?,?,?,?,?,?,?,?,?,?,?)";
             //Conexao com o Banco
             PreparedStatement stmt = conn.prepareStatement(sql);
@@ -36,8 +36,7 @@ public class ClientesDAO {
             stmt.setString(11, obj.getBairro());
             stmt.setString(12, obj.getCidade());
             stmt.setString(13, obj.getEstado());
-            
-            //EXEC
+
             stmt.execute();
             //finalizar conexão
             stmt.close();
@@ -46,9 +45,53 @@ public class ClientesDAO {
             JOptionPane.showMessageDialog(null,"Erro ao salvar o cliente" +erro);
         }
     }
+    
+        public void Editar(Clientes obj){
+        try {
+            //Insert SQL
+            String sql = "update clientes set nome=?,rg=?,cpf=?,email=?,telefone=?,celular=?,cep=?,endereco=?,numero=?,complemento=?,bairro=?,cidade=?,estado=? where id=?";
+            //Conexao com o Banco
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, obj.getNome());
+            stmt.setString(2, obj.getRg());
+            stmt.setString(3, obj.getCpf());
+            stmt.setString(4, obj.getEmail());
+            stmt.setString(5, obj.getTelefone());
+            stmt.setString(6, obj.getCelular());
+            stmt.setString(7, obj.getCep());
+            stmt.setString(8, obj.getEndereco());
+            stmt.setInt(9, obj.getNumero());
+            stmt.setString(10, obj.getComplemento());
+            stmt.setString(11, obj.getBairro());
+            stmt.setString(12, obj.getCidade());
+            stmt.setString(13, obj.getEstado());
+            stmt.setInt(14, obj.getId());
+            
+            stmt.execute();
+            //executa e finaliza
+            stmt.close();
+            JOptionPane.showMessageDialog(null, "Cliente Editado Com Sucesso");
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null,"Erro ao editar o cliente" +erro);
+        }
+    }
+        
+        public void Excluir(Clientes obj){
+            try {
+                String sql = "delete from clientes where id=?";
+                PreparedStatement stmt = conn.prepareStatement(sql);
+                stmt.setInt(1, obj.getId());
+                //executa e finaliza
+                stmt.execute();
+                stmt.close();
+                JOptionPane.showMessageDialog(null, "Cliente Excluido Com Sucesso");
+        } catch (SQLException erro) {
+                JOptionPane.showMessageDialog(null,"Erro ao excluir o cliente" +erro);
+            }
+        }
     public Clientes BuscarCliente(String nome){
         try {
-            String sql = "select * from tb_clientes where nome =?";
+            String sql = "select * from clientes where nome =?";
             PreparedStatement stmt = conn.prepareStatement(sql);
             stmt.setString(1, nome);
             ResultSet rs = stmt.executeQuery();
@@ -75,11 +118,46 @@ public class ClientesDAO {
         }
         return null;
     }
+    // LISTAR CLIENTES NO CONSULTA CLIENTES
     public List<Clientes>listar(){
         List<Clientes>lista = new ArrayList<>();
         try {
-            String sql = "select * from tb_clientes";
+            String sql = "select * from clientes";
             PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            
+            while(rs.next()){
+                Clientes obj = new Clientes();
+                    obj.setId(rs.getInt("id"));
+                    obj.setNome(rs.getString("nome"));
+                    obj.setRg(rs.getString("rg"));
+                    obj.setCpf(rs.getString("cpf"));
+                    obj.setEmail(rs.getString("email"));
+                    obj.setTelefone(rs.getString("telefone"));
+                    obj.setCelular(rs.getString("celular"));
+                    obj.setCep(rs.getString("cep"));
+                    obj.setEndereco(rs.getString("endereco"));
+                    obj.setNumero(rs.getInt("numero"));
+                    obj.setComplemento(rs.getString("complemento"));
+                    obj.setBairro(rs.getString("bairro"));
+                    obj.setCidade(rs.getString("cidade"));
+                    obj.setEstado(rs.getString("estado"));
+                    lista.add(obj);
+            }
+            return lista;
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro Ao Criar a Lista" + erro);
+        }
+        return null;
+    }
+    
+        // PESQUISA CLIENTES NO BOTAO PESQUISAR
+    public List<Clientes>filtrar(String nome){
+        List<Clientes>lista = new ArrayList<>();
+        try {
+            String sql = "select * from clientes where nome like ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, nome);
             ResultSet rs = stmt.executeQuery();
             
             while(rs.next()){
